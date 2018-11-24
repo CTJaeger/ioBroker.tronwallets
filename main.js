d949b0fa60429689e68eee1b4bdb42bc492c435e
CTJaeger <info@ct-j.de>
2018-11-24 15:52:39 +0100

    Add files via upload
    refactoring
    json

--- main.js
+++ main.js
@@ -3,14 +3,18 @@
 'use strict';
 
 const utils =   require(__dirname + '/lib/utils'); // Get common adapter utils
+const XMLHttp = require('xmlhttprequest');
 const request = require('request');
 const adapter = new utils.Adapter('tronwallets');
 const trxusdt = 'https://api.binance.com/api/v3/ticker/price?symbol=TRXUSDT';
 const trxbtc  = 'https://api.binance.com/api/v3/ticker/price?symbol=TRXBTC';
 const btcusdt = 'https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT';
 
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+var xhr = new XMLHttpRequest();
+var jso = '';
 var result;
-var summe;
+var summe = 0;
 var err;
 var ip;
 var timer     = null;
@@ -60,7 +64,7 @@ adapter.on('ready', function () {
         unit: 'BTC'
     });
 
-        main();
+    main();
 });
 
 process.on('SIGINT', function () {
@@ -73,151 +77,134 @@ function main() {
         adapter.log.error('No wallets found');
         return;
     }
-    adapter.config.interval   = parseInt(adapter.config.interval,   10) || 30000;
+    adapter.config.interval = parseInt(adapter.config.interval, 10) || 30000;
     if (adapter.config.interval < 5000) {
         adapter.config.interval = 5000;
     }
 
-    setInterval(abruf, adapter.config.interval || 5000);
-}
-
-    function abruf() {
+    //setInterval(abruf, adapter.config.interval || 5000);
+    setInterval(abruf, 20000);
+    setInterval(exchange, 30000);
     exchange();
-
-    var jso = '';
-    var request = require('request');
+}
+function abruf() {
     var werte;
     for (var u = 0; u < adapter.config.devices.length; u++) {
 
         const name = adapter.config.devices[u].name
         const ip = adapter.config.devices[u].ip
 
-        getBody(url + ip, function (err, body) {
-            if (err) {
-                adapter.log.debug('Fehler beim Abruf der Wallet' + name);
-            } else {
-                try {
-                    werte = JSON.parse(body);
-                } catch (e) {
-                    adapter.log.error ('Parsen der Wallet fehlgeschlagen' + e);
-                }
-                try {
-                adapter.setState(adapter.namespace + '.' + name + '.name', werte.name);
-                adapter.setState(adapter.namespace + '.' + name + '.balance', werte.balance);
-                adapter.setState(adapter.namespace + '.' + name + '.address', werte.address);
-                adapter.setState(adapter.namespace + '.' + name + '.bandwith', werte.bandwith);
-                adapter.setState(adapter.namespace + '.' + name + '.allowance', werte.allowance);
-                adapter.setState(adapter.namespace + '.' + name + '.frozenBalance', werte.frozenBalance);
-                adapter.setState(adapter.namespace + '.' + name + '.totalBalance', werte.totalBalance);
-                adapter.setState(adapter.namespace + '.' + name + '.frozenExpire', werte.frozenExpire);
-                adapter.setState(adapter.namespace + '.' + name + '.createTime', werte.createTime);
-                adapter.setState(adapter.namespace + '.' + name + '.usdValue', werte.usdValue);
-                adapter.setState(adapter.namespace + '.' + name + '.transferFromCount', werte.transferFromCount);
-                adapter.setState(adapter.namespace + '.' + name + '.transferToCount', werte.transferToCount);
-                adapter.setState(adapter.namespace + '.' + name + '.tokensCount', werte.tokensCount);
-                adapter.setState(adapter.namespace + '.' + name + '.participationsCount', werte.participationsCount);
-                adapter.setState(adapter.namespace + '.' + name + '.tokensCount', werte.tokensCount);
-                adapter.setState(adapter.namespace + '.' + name + '.balanceStr', werte.balanceStr);
-                adapter.setState(adapter.namespace + '.' + name + '.frozenBalanceStr', werte.frozenBalanceStr);
-                adapter.setState(adapter.namespace + '.' + name + '.totalBalanceStr', werte.totalBalanceStr);
-                adapter.setState(adapter.namespace + '.' + name + '.bandwidthStr', werte.bandwidthStr);
-                adapter.setState(adapter.namespace + '.' + name + '.allowanceStr', werte.allowanceStr);
-                } catch (e) {
-                    stateserstellen();
-                }
+        xhr.open("GET", url + ip, false);
+        xhr.responseType = 'json';
+        xhr.send(null);
 
-            }
-        });
-        // Tokenbestand abrufen
-        getBody(url2 + ip, function (err, body) {
-            if (err) {
-                adapter.log.debug('Fehler beim Abruf des Tokenbestand.');
-            } else {
-                // Array im Array parsen
-                try {
-                    var token = JSON.parse(body).data.assets;
-                } catch (e) {
-                    adapter.log.error ('Parsen der Token fehlgeschlagen' + e);
-                }
-                summe = 0;
-                for (var key in token) {
-
-                    var price = round(getprice(key),6);
-                    summe = summe + (price * token[key]);
-                    try {
-                        adapter.setState(adapter.namespace + '.' + name + '.Token.' + key, token[key]);
-                        adapter.setState(adapter.namespace + '.' + name + '.Token.' + key + '-TRX', price);
-                    } catch (e) {
-                        adapter.createState(name, 'Token', key, 'value');
-                        adapter.createState(name ,'Token', key + '-TRX','value');
-                    }
-
-                }
-                adapter.setState(adapter.namespace + '.' + name + '.Tokenbalance', round(summe,6));
-                adapter.log.debug(name + "->" + summe);
-            }
-
-        });
-        //summe = 0;
-    }
-    bina();
-        };
-    // Kurse von Binance abrufen
-    function bina() {
         try {
-            request(btcusdt, function (err, stat, body) {
-                var werte = JSON.parse(body);
-                adapter.setState(adapter.namespace + '.General.BTCUSDT', werte.price);
-            });
-            request(trxusdt, function (err, stat, body) {
-                var werte = JSON.parse(body);
-                adapter.setState(adapter.namespace + '.General.TRXUSDT', werte.price);
-            });
-            request(trxbtc, function (err, stat, body) {
-                var werte = JSON.parse(body);
-                adapter.setState(adapter.namespace + '.General.TRXBTC', werte.price);
-            });
+            werte = JSON.parse(xhr.responseText);
+            var pfad =adapter.namespace + '.' + name;
+
+            statee( pfad, "name", werte.name );
+            statee( pfad, "address", werte.address );
+            statee( pfad, "bandwith", werte.bandwith );
+            statee( pfad, "allowance", werte.allowance );
+            statee( pfad, "frozenBalance", werte.frozenBalance );
+            statee( pfad, "totalBalance", werte.totalBalance );
+            statee( pfad, "frozenExpire", werte.frozenExpire );
+            statee( pfad, "createTime", werte.createTime );
+            statee( pfad, "usdValue", werte.usdValue );
+            statee( pfad, "transferFromCount", werte.transferFromCount );
+            statee( pfad, "transferToCount", werte.transferToCount );
+            statee( pfad, "tokensCount", werte.tokensCount );
+            statee( pfad, "participationsCount", werte.participationsCount );
+            statee( pfad, "balanceStr", werte.balanceStr );
+            statee( pfad, "frozenBalanceStr", werte.frozenBalanceStr );
+            statee( pfad, "totalBalanceStr", werte.totalBalanceStr );
+            statee( pfad, "bandwidthStr", werte.bandwidthStr );
+            statee( pfad, "allowanceStr", werte.allowanceStr );
+
         } catch (e) {
-            adapter.log.error ('Abrufen der Preise von Binance fehlgeschlagen' + e);
+            adapter.log.error ('Parsen der Wallet fehlgeschlagen' + e);
         }
 
+        // Tokenbestand abrufen
+        xhr.open("GET", url2 + ip, false);
+        xhr.responseType = 'json';
+        xhr.send(null);
+        // Array im Array parsen
+        try {
+            var token = JSON.parse(xhr.responseText).data.assets;
 
+        } catch (e) {
+            adapter.log.error ('Parsen der Token fehlgeschlagen' + e);
+        }
+        jso = '[';
+        var counter = 0;
+        for (var key in token) {
+            if (counter > 0) {
+                jso = jso + ',';
+            }
+            var price = round(getprice(key),6);
+            summe = summe + (price * token[key]);
+
+                var pfad = adapter.namespace + '.' + name + '.Token';
+                statee(pfad, key , token[key]);
+                statee(pfad, key + '-TRX', price);
+
+                jso = jso + '{"Token" : "' + key + '" , "Price" : ' + price + ' ,"Amount" : ' + token[key] + ',"Total Balance" : ' + round(price * token[key], 6) + '}';
+                counter = counter + 1;
+
+        }
+        jso = jso + ']';
+        var pfad = adapter.namespace + '.' + name + '.Token';
+        statee(pfad, '0Tokenbalance' , summe);
+        statee(pfad, '0JSON' , jso);
+        summe = 0;
+        //jso = '';
     }
 
-    function exchange() {
-    getBody(url3, function (err, body) {
-        if (err) {
-            adapter.log.debug('Fehler beim Abruf der Exchange Contracts');
-        } else {
-            dex = JSON.parse(body);
-            }
+    bina();
+};
+// Kurse von Binance abrufen
+function bina() {
+    try {
+        request(btcusdt, function (err, stat, body) {
+            var werte = JSON.parse(body);
+            adapter.setState(adapter.namespace + '.General.BTCUSDT', werte.price);
         });
+        request(trxusdt, function (err, stat, body) {
+            var werte = JSON.parse(body);
+            adapter.setState(adapter.namespace + '.General.TRXUSDT', werte.price);
+        });
+        request(trxbtc, function (err, stat, body) {
+            var werte = JSON.parse(body);
+            adapter.setState(adapter.namespace + '.General.TRXBTC', werte.price);
+        });
+    } catch (e) {
+        adapter.log.error ('Abrufen der Preise von Binance fehlgeschlagen' + e);
     }
 
-    function getprice(token) {
-        for (var cd = 0; cd < dex.exchanges.length; cd++) {
-            var stto = hex2a(dex.exchanges[cd].first_token_id);
-                if (stto === token) {
-                    var stc = dex.exchanges[cd].second_token_balance / 1000000;
-                    var ftc = dex.exchanges[cd].first_token_balance;
-                    var priced = stc / ftc;
-                    return (priced);
-                }
-            }
-        }
-
-    function getBody(url, callback) {
-    request({
-        url: url,
-    }, function (error, response, body) {
-        if (error || response.statusCode !== 200) {
-            return callback(error || {statusCode: response.statusCode});
-        }
-        callback(null, body);
-    });
 }
-    // Funktion zur Konvertierung von Hex in ASCII
-    function hex2a(hexx) {
+
+function exchange() {
+    xhr.open("GET", url3, false);
+    xhr.responseType = 'json';
+    xhr.send(null);
+    dex = JSON.parse(xhr.responseText);
+}
+
+function getprice(token) {
+    for (var cd = 0; cd < dex.exchanges.length; cd++) {
+        var stto = hex2a(dex.exchanges[cd].first_token_id);
+        if (stto === token) {
+            var stc = dex.exchanges[cd].second_token_balance / 1000000;
+            var ftc = dex.exchanges[cd].first_token_balance;
+            var priced = stc / ftc;
+            return (priced);
+        }
+    }
+}
+
+// Funktion zur Konvertierung von Hex in ASCII
+function hex2a(hexx) {
     var hex = hexx.toString(); //force conversion
     var str = '';
     for (var i = 0; (i < hex.length && hex.substr(i, 2) !== '00'); i += 2)
@@ -225,41 +212,24 @@ function main() {
     return str;
 }
 // Funktion zur Rundung der Werte
-    function round(wert, dez) {
-        wert = parseFloat(wert);
-        if (!wert) return 0;
-        dez = parseInt(dez);
-        if (!dez) dez=0;
-        var umrechnungsfaktor = Math.pow(10,dez);
-        return Math.round(wert * umrechnungsfaktor) / umrechnungsfaktor;
+function round(wert, dez) {
+    wert = parseFloat(wert);
+    if (!wert) return 0;
+    dez = parseInt(dez);
+    if (!dez) dez=0;
+    var umrechnungsfaktor = Math.pow(10,dez);
+    return Math.round(wert * umrechnungsfaktor) / umrechnungsfaktor;
 }
-    function stateserstellen() {
-        // Create State
-        for (var u = 0; u < adapter.config.devices.length; u++) {
-            //adapter.log.info(adapter.config.devices[u].name);
-            const name = adapter.config.devices[u].name
-            const ip = adapter.config.devices[u].ip
-            adapter.createState(null, name, 'Tokenbalance', 'value');
-            adapter.createState(null, name, 'name', 'value');
-            adapter.createState(null, name, 'balance', 'value');
-            adapter.createState(null, name, 'address', 'value');
-            adapter.createState(null, name, 'bandwith', 'value');
-            adapter.createState(null, name, 'allowance', 'value');
-            adapter.createState(null, name, 'frozenBalance', 'value');
-            adapter.createState(null, name, 'totalBalance', 'value');
-            adapter.createState(null, name, 'frozenExpire', 'value');
-            adapter.createState(null, name, 'createTime', 'value');
-            adapter.createState(null, name, 'usdValue', 'value');
-            adapter.createState(null, name, 'transferFromCount', 'value');
-            adapter.createState(null, name, 'transferToCount', 'value');
-            adapter.createState(null, name, 'tokensCount', 'value');
-            adapter.createState(null, name, 'participationsCount', 'value');
-            adapter.createState(null, name, 'tokensCount', 'value');
-            adapter.createState(null, name, 'balanceStr', 'value');
-            adapter.createState(null, name, 'frozenBalanceStr', 'value');
-            adapter.createState(null, name, 'totalBalanceStr', 'value');
-            adapter.createState(null, name, 'bandwidthStr', 'value');
-            adapter.createState(null, name, 'allowanceStr', 'value');
-        };
-
-    }
+function statee(Pfad, Name , Wert) {
+    adapter.setObjectNotExists(Pfad + '.'+ Name, {
+        type: 'state',
+        common: {
+            name: Name,
+            read: true,
+            write: true,
+            value: Wert
+        },
+        native: {}
+    });
+    adapter.setState(Pfad + '.'+ Name, Wert);
+}
